b5fbf81cda74b0d49769b00718ef2b19ba5fb192
habibrifqi <habib.rifqi.16@gmail.com>
2019-04-14 19:09:54 +0700

    memberclass

---
+++ LatihanMemberClass/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek taxi
+            taxi taxi = new taxi();
+
+            //pengesetan nilai properties
+            taxi.Drivename = "jono";
+            taxi.Onduty = true;
+            taxi.NumberofPassenger = 10;
+
+            //pemangilan method
+            taxi.registrasi();
+            taxi.req();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LatihanMemberClass/taxi.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass {
+    class taxi {
+        public string Drivename { get; set; }
+        public bool Onduty { get; set; }
+        public int NumberofPassenger { get; set; }
+
+        public void registrasi() {
+            Console.WriteLine("Drive name : {0}", Drivename);
+            Console.WriteLine("Onduty Duty:{0}", Onduty ? "yes" : "No");
+            Console.WriteLine("Number of passenger: {0}", NumberofPassenger);
+
+
+        }
+        public void req() {
+            Console.WriteLine("{0} sedang menjemput penumpang ", Drivename);
+
+
+        }
+    }
+}
